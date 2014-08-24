dae528f839661e41fbaff55b7d8718ac9c587eb3
Susan Prestage <susan.prestage@gmail.com>
2014-08-24 08:30:30 -0700

    Improve test case syntax.

--- no_odds_test.rb
+++ no_odds_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'no_odds'
 
-class NoOddsTest < MiniTest::Unit::TestCase
+class NoOddsTest < MiniTest::Test
   def test_1
     assert_equal [2,4,6,8,0], no_odds([1,2,3,4,5,6,7,8,9,0])
   end
